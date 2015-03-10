52d21a6cb556564fcfc3cd8d4d38bbf86773aa5e
Angus Lai <amagicube@gmail.com>
2015-03-10 19:17:23 -0400

    added time and date functions

--- app/src/main/java/com/yask/android/photorocket/NewEventActivity.java
+++ app/src/main/java/com/yask/android/photorocket/NewEventActivity.java
@@ -10,8 +10,22 @@ import android.view.View;
 import android.view.ViewGroup;
 import java.util.Date;
 
+import android.support.v4.app.DialogFragment;
+import android.app.TimePickerDialog;
+import android.app.DatePickerDialog;
+import java.util.Calendar;
+import android.app.Dialog;
+import android.text.format.DateFormat;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.DatePicker;
+import android.app.Activity;
 
-public class NewEventActivity extends ActionBarActivity {
+
+
+
+
+public class NewEventActivity extends ActionBarActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +61,8 @@ public class NewEventActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+
+    /*=*/ //use this to save event
     //Helper function to save an Event
     private void saveEvent(String eventName, Date startTime, Date endTime){
         Event event = new Event(eventName,startTime,endTime);
@@ -68,4 +84,235 @@ public class NewEventActivity extends ActionBarActivity {
             return rootView;
         }
     }
+
+//----------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------
+// setting start and end time and date
+
+    private static int startHr = -1;
+    private static int startMin = -1;
+
+
+    public void showStartTimePickerDialog(View v) {
+        DialogFragment newFragment = new StartTimePickerFragment(v);
+        newFragment.show(getSupportFragmentManager(), "timePicker");
+    }
+
+    public static class StartTimePickerFragment extends DialogFragment
+            implements TimePickerDialog.OnTimeSetListener {
+
+        private View v;
+
+        public StartTimePickerFragment(View v){
+            super();
+            this.v = v;
+        }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current time as the default values for the picker
+            final Calendar c = Calendar.getInstance();
+            int hour = c.get(Calendar.HOUR_OF_DAY);
+            int minute = c.get(Calendar.MINUTE);
+
+            // Create a new instance of TimePickerDialog and return it
+            return new TimePickerDialog(getActivity(), this, hour, minute,
+                    DateFormat.is24HourFormat(getActivity()));
+        }
+
+        public void onTimeSet(TimePicker view, int hr, int min) {
+            TextView st = (TextView) v.findViewById(R.id.startTime);
+            startHr = hr;
+            startMin = min;
+            String strhr;
+            String strmin;
+            if (hr < 10){
+                strhr = "0" + hr;
+            } else {
+                strhr = "" + hr;
+            }
+            if (min < 10){
+                strmin = "0" + min;
+            } else {
+                strmin = "" + min;
+            }
+            st.setText(strhr + ":" + strmin);
+
+        }
+    }
+
+    private static int endHr = -1;
+    private static int endMin = -1;
+
+
+    public void showEndTimePickerDialog(View v) {
+        DialogFragment newFragment = new EndTimePickerFragment(v);
+        newFragment.show(getSupportFragmentManager(), "timePicker");
+    }
+
+    public static class EndTimePickerFragment extends DialogFragment
+            implements TimePickerDialog.OnTimeSetListener {
+
+        private View v;
+
+        public EndTimePickerFragment(View v){
+            super();
+            this.v = v;
+        }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current time as the default values for the picker
+            final Calendar c = Calendar.getInstance();
+            int hour = c.get(Calendar.HOUR_OF_DAY);
+            int minute = c.get(Calendar.MINUTE);
+
+            // Create a new instance of TimePickerDialog and return it
+            return new TimePickerDialog(getActivity(), this, hour, minute,
+                    DateFormat.is24HourFormat(getActivity()));
+        }
+
+        public void onTimeSet(TimePicker view, int hr, int min) {
+            TextView et = (TextView) v.findViewById(R.id.endTime);
+            endHr = hr;
+            endMin = min;
+            String strhr;
+            String strmin;
+            if (hr < 10){
+                strhr = "0" + hr;
+            } else {
+                strhr = "" + hr;
+            }
+            if (min < 10){
+                strmin = "0" + min;
+            } else {
+                strmin = "" + min;
+            }
+            et.setText(strhr + ":" + strmin);
+        }
+    }
+
+
+    private static int startYr = -1;
+    private static int startMn = -1;
+    private static int startDt = -1;
+
+
+    public void showStartDatePickerDialog(View v) {
+        DialogFragment newFragment = new StartDatePickerFragment(v);
+        newFragment.show(getSupportFragmentManager(), "timePicker");
+    }
+
+    public static class StartDatePickerFragment extends DialogFragment
+            implements DatePickerDialog.OnDateSetListener {
+
+        private View v;
+
+        public StartDatePickerFragment(View v){
+            super();
+            this.v = v;
+        }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            // Do something with the date chosen by the user
+            TextView sd = (TextView) v.findViewById(R.id.startDate);
+            startYr = year;
+            startMn = month;
+            startDt = day;
+            String strmn;
+            String strdt;
+            if (day < 10){
+                strdt = "0" + day;
+            } else {
+                strdt = "" + day;
+            }
+            if (month < 9){
+                strmn = "0" + (month + 1);
+            } else {
+                strmn = "" + (month + 1);
+            }
+            sd.setText(year + "-" + strmn + "-" + strdt);
+        }
+
+    }
+
+
+
+    private static int endYr = -1;
+    private static int endMn = -1;
+    private static int endDt = -1;
+
+
+    public void showEndDatePickerDialog(View v) {
+        DialogFragment newFragment = new EndDatePickerFragment(v);
+        newFragment.show(getSupportFragmentManager(), "timePicker");
+    }
+
+    public static class EndDatePickerFragment extends DialogFragment
+            implements DatePickerDialog.OnDateSetListener {
+
+        private View v;
+
+        public EndDatePickerFragment(View v){
+            super();
+            this.v = v;
+        }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            // Do something with the date chosen by the user
+            TextView ed = (TextView) v.findViewById(R.id.endDate);
+            endYr = year;
+            endMn = month;
+            endDt = day;
+            String strmn;
+            String strdt;
+            if (day < 10){
+                strdt = "0" + day;
+            } else {
+                strdt = "" + day;
+            }
+            if (month < 9){
+                strmn = "0" + (month + 1);
+            } else {
+                strmn = "" + (month + 1);
+            }
+            ed.setText(year + "-" + strmn + "-" + strdt);
+        }
+    }
+
+//----------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------
+// create event
+    public void processCreate(View v){
+        if (startDt < 0 || startMin < 0 || endDt < 0 || endMin < 0){
+            //invalid date / time
+        } else {
+
+        }
+    }
+
 }
