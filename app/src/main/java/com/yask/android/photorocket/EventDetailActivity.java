6858d1bb2f37fce8b9bb7a1c7ad364685ecfd112
Sen Chang <tonych@mit.edu>
2015-04-12 16:34:30 -0400

    enlarged photos are now working

--- app/src/main/java/com/yask/android/photorocket/EventDetailActivity.java
+++ app/src/main/java/com/yask/android/photorocket/EventDetailActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.GridView;
 
 import com.parse.FindCallback;
