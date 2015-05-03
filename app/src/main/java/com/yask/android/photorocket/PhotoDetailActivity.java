6d0ae0de4130c8de3a597d3c3681e5b107bf938c
Kapaya <kkatongo@mit.edu>
2015-05-03 16:52:10 -0400

    changed title

--- app/src/main/java/com/yask/android/photorocket/PhotoDetailActivity.java
+++ app/src/main/java/com/yask/android/photorocket/PhotoDetailActivity.java
@@ -1,16 +1,11 @@
 package com.yask.android.photorocket;
 
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 
 
 public class PhotoDetailActivity extends ActionBarActivity {
@@ -19,6 +14,7 @@ public class PhotoDetailActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo_detail);
+        setTitle("Photo");
     }
 
    @Override
